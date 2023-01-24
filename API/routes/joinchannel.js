7dc438b63cc0ac108b3c27f5075534695fa308d8
Ruben <85025254+Kaylua@users.noreply.github.com>
2023-01-24 23:20:42 +0100

    rename bot

--- API/routes/joinchannel.js
+++ API/routes/joinchannel.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const functions = require('../../Bot/functions')
+const functions = require('../../BOT/functions')
 const discord = require('../../BOT/discordmain')
 
 router.post('', async (req, res) => {
--- API/routes/unban.js
+++ API/routes/unban.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const functions = require('../../Bot/functions')
+const functions = require('../../BOT/functions')
 const discord = require('../../BOT/discordmain')
 const logs = require('../DB_schemas/logs')
 
